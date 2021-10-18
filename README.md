22ea963a31b92473e4c4f9d61eda25c1b9661154
pengge <caipeng8@gmail.com>
2021-10-18 12:51:40 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# wgis.leaflet.markercluster.vue2
+Leaflet.markercluster 
+=====================
+modfiy  Leaflet.markercluster  
+
+### Building, testing and linting scripts
+Install jake `npm install -g jake` then run `npm install`
+* To check the code for errors and build Leaflet from source, run `npx jake`.
+* To run the tests, run `npx jake test`.
---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+
+import {} from './DistanceGrid.js';
+export { MarkerClusterGroup } from './MarkerClusterGroup.js';
+export { MarkerCluster } from './MarkerCluster.js';
+import {} from './MarkerOpacity.js';
+import {} from './MarkerCluster.QuickHull.js';
+import {} from './MarkerCluster.Spiderfier.js';
+import {} from './MarkerClusterGroup.Refresh.js';
